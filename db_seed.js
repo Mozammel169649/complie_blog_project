08a16e372099242271dbc5f944d274cc92331451
Mozammel169649 <mozammel169649@gmail.com>
2023-10-22 16:06:54 +0600

    seed connebtion with database

---
+++ db_seed.js
@@ -0,0 +1,22 @@
+// const blogSedder = require("./app/models/sedders/blog.sedder");
+// const categorySedder = require("./app/models/sedders/category.sedder");
+// const translatorSedder = require("./app/models/sedders/translator.sedder");
+// const userSedder = require("./app/models/sedders/user.sedder");
+// const writerSedder = require("./app/models/sedders/writer.sedder");
+
+// const mediaSeeder = require("./app/models/sedders/media.seeder");
+
+// const settingSedder = require("./app/models/sedders/setting.sedder");
+
+const seed = async()=> {
+
+    //  await settingSedder();
+   // await userSedder();
+   // await categorySedder();
+   // await writerSedder();
+   // await translatorSedder();
+   // await blogSedder();
+//    await mediaSeeder();
+}
+
+seed();
