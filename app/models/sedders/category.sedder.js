b02b759515dca4972de2b9262298d5f92bc9991d
Mozammel169649 <mozammel169649@gmail.com>
2023-10-22 16:09:52 +0600

    set category seeder

---
+++ app/models/sedders/category.sedder.js
@@ -0,0 +1,43 @@
+const { default: mongoose, Schema } = require("mongoose");
+const categoryModel = require("../category.model");
+const { db_url } = require("../../../configs/db.config");
+
+
+
+let categories = [
+	{
+		title: "Opinion",
+		creator: "64c8470fa9aaec4787ca3a97",
+	},
+	{
+		title: "Sports",
+		creator: "64c8470fa9aaec4787ca3a98",
+		status:false,
+	},
+	{
+		title: "Business",
+		creator: "64c8470fa9aaec4787ca3a98",
+	},
+	{
+		title: "Entertainment",
+		creator: "64c8470fa9aaec4787ca3a97",
+	},
+];
+
+module.exports = () =>
+	mongoose.connect(db_url)
+		.then(async () => {
+
+			await categoryModel.deleteMany({});
+			let response = await categoryModel.insertMany(categories);
+
+			// for (let i = 0; i < 50; i++) {
+			// 	await categoryModel.create({
+			// 		title : "category"+ i,
+			// 		creator : "64c8470fa9aaec4787ca3a98"
+			// 	})
+			// }
+
+			console.log("catetgory seeding complete");
+
+		});
