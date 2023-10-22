2724a1a1153f298056a3795a194df6f169a054e4
Mozammel169649 <mozammel169649@gmail.com>
2023-10-22 16:11:09 +0600

    user seeder

---
+++ app/models/sedders/user.sedder.js
@@ -0,0 +1,26 @@
+const { default: mongoose, Schema } = require("mongoose");
+const { db_url } = require("../../../configs/db.config");
+const userModel = require("../user.model");
+
+let users = [
+	{
+		userName: "admin",
+		email: "admin@gmail.com",
+		password:"$2b$12$7/AEv2vyK3ezRF6tL15ws.EDOSva1qdV2W4V3z52IgXFo9eknFTH.",
+	},
+	{
+		userName: "user",
+		email: "user@gmail.com",
+		password: "$2b$12$Zo8Spr3UNF9T0KSoWbmuTeXIj.L984HJ8Fh50Z/rmxdKnylFV13ia",
+	},
+];
+
+module.exports = () =>
+	mongoose.connect(db_url)
+		.then(async () => {
+
+			await userModel.deleteMany({});
+			await userModel.insertMany(users);
+
+			console.log("user seeding complete");
+		});
