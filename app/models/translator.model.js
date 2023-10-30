7a35628fef98986f033aaa987dc40daff627c87f
Mozammel169649 <mozammel169649@gmail.com>
2023-10-30 14:32:37 +0600

    translator model

---
+++ app/models/translator.model.js
@@ -0,0 +1,32 @@
+const { default: mongoose, Schema } = require("mongoose");
+const userModel = require("./user.model");
+
+module.exports = mongoose.model(
+	"translators",
+	mongoose.Schema(
+		{
+			name: {
+				type: String,
+				required: true,
+			},
+			description: {
+				type: String,
+			},
+			image: {
+				type: String,
+			},
+			creator: {
+				type: Schema.Types.ObjectId,
+				required: true,
+				ref: userModel,
+			},
+			status: {
+				type: Boolean,
+				default: true,
+			},
+		},
+		{
+			timestamps: true,
+		}
+	)
+);
