f5cd853ee2956e966150f449dd7184c12f9dccfe
Mozammel169649 <mozammel169649@gmail.com>
2023-10-30 14:32:54 +0600

    user model

---
+++ app/models/user.model.js
@@ -0,0 +1,21 @@
+const { default: mongoose } = require("mongoose");
+
+module.exports = mongoose.model(
+    'users', mongoose.Schema({
+        userName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+        },
+        password: {
+            type: String,
+        },
+        role: {
+            type: String,
+            default: 'user',
+        }
+    }, {
+        timestamps: true,
+    }));
