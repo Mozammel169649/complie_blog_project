aeb08d9a2d2a38846d7c8e04091d83cd222dd0b4
Mozammel169649 <mozammel169649@gmail.com>
2023-10-31 11:22:07 +0600

    category count middleware

---
+++ app/middelware/global.middleware.js
@@ -0,0 +1,35 @@
+const blogModel = require("../models/blog.model");
+const categoryModel = require("../models/category.model");
+const settingModel = require("../models/setting.model");
+
+module.exports = async (server, req, res, next) => {
+    let setting = await settingModel.find();		
+    let recent2post = await blogModel.find().sort({createdAt : -1}).limit(2).populate('creator').populate('category');
+    let categories = await categoryModel.find(); 
+
+
+    let categoryView = await categoryModel.find();
+	categoryView = await count_post(categoryView);
+
+	 async function count_post(els) {
+		let temp = [];
+		for (let index = 0; index < els.length; index++) {
+
+			let element = els[index];
+			element = { ...element }._doc;
+			element.count = await blogModel.where("category").in(element._id).count().exec();
+			temp.push(element)
+		}
+		return temp
+
+	};
+
+
+    server.locals.categoryView = categoryView ;
+    server.locals.setting = setting ;
+    server.locals.categories = categories ;
+    server.locals.recent2post = recent2post ;
+
+   
+    
+}
