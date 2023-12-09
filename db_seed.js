6ebc407fb74a11ca341fa7acb61ed31f258bbbe9
Mozammel169649 <mozammel169649@gmail.com>
2023-12-09 18:16:25 +0600

    database sedder

--- db_seed.js
+++ db_seed.js
@@ -1,22 +1,20 @@
-// const blogSedder = require("./app/models/sedders/blog.sedder");
-// const categorySedder = require("./app/models/sedders/category.sedder");
-// const translatorSedder = require("./app/models/sedders/translator.sedder");
-// const userSedder = require("./app/models/sedders/user.sedder");
-// const writerSedder = require("./app/models/sedders/writer.sedder");
-
-// const mediaSeeder = require("./app/models/sedders/media.seeder");
-
-// const settingSedder = require("./app/models/sedders/setting.sedder");
+const blogSedder = require("./app/models/sedders/blog.sedder");
+const categorySedder = require("./app/models/sedders/category.sedder");
+const translatorSedder = require("./app/models/sedders/translator.sedder");
+const userSedder = require("./app/models/sedders/user.sedder");
+const writerSedder = require("./app/models/sedders/writer.sedder");
+const mediaSeeder = require("./app/models/sedders/media.seeder");
+const settingSedder = require("./app/models/sedders/setting.sedder");
 
 const seed = async()=> {
 
-    //  await settingSedder();
-   // await userSedder();
-   // await categorySedder();
-   // await writerSedder();
-   // await translatorSedder();
-   // await blogSedder();
-//    await mediaSeeder();
+   await settingSedder();
+   await userSedder();
+   await categorySedder();
+   await writerSedder();
+   await translatorSedder();
+   await blogSedder();
+   await mediaSeeder();
 }
 
 seed();
