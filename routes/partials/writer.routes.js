80c548f24a8730908c9f216210b311ced243f288
Mozammel169649 <mozammel169649@gmail.com>
2023-12-24 12:11:30 +0600

    writer router

---
+++ routes/partials/writer.routes.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const isauthMiddleware = require("../../app/middelware/isauth.middleware");
+const { writerController } = require("../../app/controllers/backend/writer.controller");
+const writerModel = require("../../app/models/writer.model");
+
+const router = express.Router();
+
+router
+    .get("/dashboard/writer/all" , writerController.all)
+    .get("/dashboard/writer/:id/edit", writerController.edit)
+    .get("/dashboard/writer/:id/show", writerController.show)
+    .post('/dashboard/writer/update-data-submit/:id' , writerController.editSubmit)
+    .get("/dashboard/writer/:id/delet", writerController.delete)
+
+    .get("/dashboard/writer/creator" , writerController.create)
+    .post("/dashboard/writer_submit" , writerController.store)
+    .post("/dashboard/writer/set-status", writerController.set_status)
+
+    .post("/dashboard/writer/deletSelectedItem", writerController.deletSelectedItem)
+    .post("/dashboard/writer/for-Export-ids", writerController.export_items)
+    
+
+module.exports = () => router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // .post("/dashboard/translator/deletSelectedItem", translatorControllers.deletSelectedItem)
+    // .post("/dashboard/translator/for-Export-ids", translatorControllers.export_items)
+
