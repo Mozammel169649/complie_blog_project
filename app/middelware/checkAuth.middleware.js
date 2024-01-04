7e9a588ccf06f18d72068dba1ee88d7b1e5927e0
Mozammel169649 <mozammel169649@gmail.com>
2024-01-04 09:26:31 +0600

    checkAuth middleware

--- app/middelware/checkAuth.middleware.js
+++ app/middelware/checkAuth.middleware.js
@@ -1,15 +1,20 @@
+
 var jwt = require('jsonwebtoken');
 module.exports = async (server, req, res, next) => {
+
     const { atoken } = req.cookies;
     server.locals.checkIsAuth = false;
     server.locals.isAdmin = false;
     server.locals.user = {};
+    
     if (atoken) {
         try{
             const decoded = await jwt.verify(atoken, '6fd286f7-708a-429b-b53a-2bc5272e0db6');
             server.locals.checkIsAuth = true;
             server.locals.user = decoded;
-            server.locals.isAdmin = decoded.role==='admin'?true:false;
+
+            server.locals.isAdmin = decoded.role == true?true:false;
+
             req.session.user = decoded;
         } catch (error){
             server.locals.checkIsAuth = false;
