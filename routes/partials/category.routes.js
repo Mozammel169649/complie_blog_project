5356a696c2d5ed0fd8374a66efa8b5ff9a6e6bae
Mozammel169649 <mozammel169649@gmail.com>
2024-01-10 22:29:10 +0600

    category routes

---
+++ routes/partials/category.routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const isauthMiddleware = require('../../app/middelware/isauth.middleware');
+const { categoryControllers } = require("../../app/controllers/backend/category.controller");
+const router = express.Router();
+
+router
+	.get("/dashboard/category", categoryControllers.all)
+	.post("/dashboard/category/from-ids", categoryControllers.from_ids)
+	.post("/dashboard/category/delete-items", categoryControllers.delete_items)
+	.post("/dashboard/category/set-status", categoryControllers.set_status)
+	.get("/dashboard/category/create", categoryControllers.create)
+	.post("/dashboard/category_submit", categoryControllers.store)
+
+	.get("/dashboard/category/:id/edit", categoryControllers.edit)
+	.get("/dashboard/category/:id/delete", categoryControllers.delete)
+	.get("/dashboard/category/:id", categoryControllers.show)
+	.post("/dashboard/category/:id", categoryControllers.editSubmit)
+
+
+
+module.exports = () => router;
