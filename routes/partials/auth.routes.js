aceccb8ba0bff105919988b36616848055aa5df6
Mozammel169649 <mozammel169649@gmail.com>
2024-01-10 22:28:48 +0600

    auth routes

--- routes/partials/auth.routes.js
+++ routes/partials/auth.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const isauthMiddleware = require('../../app/middelware/isauth.middleware');
 const userModel = require('../../app/models/user.model');
 const router = express.Router()
+
 const bcrypt = require("bcrypt")
 const jwt = require('jsonwebtoken');
 
