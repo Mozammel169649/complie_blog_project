28d7b4ddc9cbe413d36556044f176ce6fc092d6b
Mozammel169649 <mozammel169649@gmail.com>
2024-01-11 23:51:07 +0600

    email routes

---
+++ routes/partials/email.routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const isauthMiddleware = require('../../app/middelware/isauth.middleware');
+const router = express.Router()
+
+router
+    .get('/dashboard/email',  function (req, res) {
+    return res.render("backend/email_management/all")
+    })
+    .get('/dashboard/email/create',  function (req, res) {
+        return res.render("backend/email_management/create")
+    })
+    .get('/dashboard/email/:id/edit',  function (req, res) {
+        return res.render("backend/email_management/edit")
+    })
+    .get('/dashboard/email/:id',  function (req, res) {
+        return res.render("backend/email_management/show")
+    });
+
+    module.exports = ()=> router;
