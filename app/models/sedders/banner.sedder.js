2ec2494b143434e663640f26705237b17e0be0d7
Mozammel169649 <mozammel169649@gmail.com>
2024-01-18 21:40:23 +0600

    banner dainamik creator setup

--- app/models/sedders/banner.sedder.js
+++ app/models/sedders/banner.sedder.js
@@ -1,13 +1,20 @@
 const { default: mongoose} = require("mongoose");
 const { db_url } = require("../../../configs/db.config");
+const categoryModel = require("../category.model");
 const bannerModel = require("../banner.model");
 
+
+
+const get_category_ids = async () => {
+	let categories = await categoryModel.find();
+	return categories.map(i => i._id.toString());
+}
 
 
 const demo_banner= async () => {
 	return {
 		title: "Men's football team finally going to Asiad",
-		category: "mozammel",
+		category: await get_category_ids(),
 		creator: "mozammel",
 		writing_date: "2023-01-25 ",
 		thumb_image: "images/1.jpg",
