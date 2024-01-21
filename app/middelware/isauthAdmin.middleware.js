c2b84b0434b7f870db3df7bfed31d4d9d442513e
Mozammel169649 <mozammel169649@gmail.com>
2024-01-21 16:12:24 +0600

    database to admin authontication

--- app/middelware/isauthAdmin.middleware.js
+++ app/middelware/isauthAdmin.middleware.js
@@ -1,6 +1,12 @@
-module.exports = (req, res, next) => {
-    if(req.session.user.role == true){
-        next();
+const userModel = require("../models/user.model");
+
+module.exports = async(req, res, next) => {
+    
+    const user = req.session.user?._id
+    const auth = await userModel.findOne().where({ _id:user })
+
+    if(auth?.role == true){
+        next()
     }else{
         return res.redirect('/');
     }
